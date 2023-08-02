fac051a0e56c05803f3e1751119d01eead92f9a3
Th3PL <plucas2013@gmail.com>
2023-08-02 09:29:24 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,8 +16,12 @@ public class Program {
 		System.out.println(obj);
 		
 		Seller s1 = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(s1);
 
+		
 	}
 
 }
