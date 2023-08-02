85472dac757b73c6ca7cb0f31286fec6f7d02e91
Th3PL <plucas2013@gmail.com>
2023-08-02 11:46:34 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,10 +34,15 @@ public class Program {
 		}
 		
 		System.out.println("\n=== TEST 4: seller insert === ");
-		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4.000, department);
 		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New id= " + newSeller.getId()
-		);
+		System.out.println("Inserted! New id= " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller update === ");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 	}
 
 }
